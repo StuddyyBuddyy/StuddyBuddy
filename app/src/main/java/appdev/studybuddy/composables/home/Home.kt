dd9f9ca376b52b9485dc098ecb92f9a5eff7f31d
eliasra-aau <eliasra@edu.aau.at>
2025-08-16 10:37:31 +0200

    fixed sizing issue

--- app/src/main/java/appdev/studybuddy/composables/home/Home.kt
+++ app/src/main/java/appdev/studybuddy/composables/home/Home.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -213,7 +214,8 @@ fun HomeScreen(
                 LazyColumn(
                     modifier = Modifier
                         //.fillMaxSize()
-                        .padding(horizontal = 16.dp),
+                        .padding(horizontal = 16.dp)
+                        .sizeIn(maxHeight = 200.dp),
                     verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
                     items(personalScoreboard.toList()) { (sessionDate, points) ->
