676ece058aa221a65756a75c6cce01b2a411c2a0
noemi-presser <noemihannah.presser@gmail.com>
2025-08-30 23:34:49 +0200

    improved buttons in session dialogs

--- app/src/main/java/appdev/studybuddy/ui/theme/Color.kt
+++ app/src/main/java/appdev/studybuddy/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleBackground = Color(0xFFCCC2DC)
 val PurpleBackground2 = Color(0xFFA372ED)
-val Pink80 = Color(0xFFCE569F)
+val Pink80 = Color(0xFFCE69A6)
 
 val Purple40 = Color(0xFF6650a4)
 val Pink40 = Color(0xFFA02A71)
@@ -15,7 +15,8 @@ val PurpleDarkText = Color(0xFF332166)
 
 val Beige = Color(0xFFFFF3DA)
 val DarkGrey = Color(0xFF2a2a2a)
-val Pink = Color(0xFFD65AA5)
+val Pink = Color(0xFFD168A7)
+val LightPink = Color(0xFFB87B9F)
 val Grey = Color(0xFF4c454c)
 
 val logOutRed = Color(0xFF710827)
