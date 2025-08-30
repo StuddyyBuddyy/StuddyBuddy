800e71fd73124292791384f38472e3c8328e301f
noemi-presser <noemihannah.presser@gmail.com>
2025-08-30 23:05:08 +0200

    improved coloring in light and dark theme

--- app/src/main/java/appdev/studybuddy/ui/theme/Color.kt
+++ app/src/main/java/appdev/studybuddy/ui/theme/Color.kt
@@ -5,17 +5,17 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleBackground = Color(0xFFCCC2DC)
 val PurpleBackground2 = Color(0xFFA372ED)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink80 = Color(0xFFCE569F)
 
 val Purple40 = Color(0xFF6650a4)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFFA02A71)
 
 val PurpleDarkText = Color(0xFF332166)
 
 
 val Beige = Color(0xFFFFF3DA)
 val DarkGrey = Color(0xFF2a2a2a)
-val Pink = Color(0xFFffc0e6)
+val Pink = Color(0xFFD65AA5)
 val Grey = Color(0xFF4c454c)
 
 val logOutRed = Color(0xFF710827)
